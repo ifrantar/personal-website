22d22703311d4f9e837b309ca6b9b170ee4bd662
Ivan Frantar <ivan@ivanfrantar.com>
2024-07-17 14:35:51 +0200

    Removing the sticky header functionality as I'm seeing some people having problems in their browsers.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,10 @@
-document.addEventListener('scroll', () => {
-    const nav = document.querySelector('nav');
-    const scrollPosition = window.scrollY;
+// document.addEventListener('scroll', () => {
+//     const nav = document.querySelector('nav');
+//     const scrollPosition = window.scrollY;
   
-    if (scrollPosition > 80) {
-      nav.classList.add('sticky-header');
-    } else if (scrollPosition < 500) {
-      nav.classList.remove('sticky-header');
-    }
-});
+//     if (scrollPosition > 80) {
+//       nav.classList.add('sticky-header');
+//     } else if (scrollPosition < 500) {
+//       nav.classList.remove('sticky-header');
+//     }
+// });

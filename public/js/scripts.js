c0b84fdafe83fc133864cdf04d214614d288359f
Ivan Frantar <ivan@ivanfrantar.com>
2024-07-18 16:46:28 +0200

    Updated the sticky header to have a smoother transition.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,24 @@
-// document.addEventListener('scroll', () => {
-//     const nav = document.querySelector('nav');
-//     const scrollPosition = window.scrollY;
-  
-//     if (scrollPosition > 80) {
-//       nav.classList.add('sticky-header');
-//     } else if (scrollPosition < 500) {
-//       nav.classList.remove('sticky-header');
-//     }
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    const nav = document.querySelector('nav');
+    
+    // Create the placeholder element
+    const navPlaceholder = document.createElement('div');
+    navPlaceholder.classList.add('nav-placeholder');
+    navPlaceholder.style.height = `${nav.offsetHeight}px`;
+    navPlaceholder.style.display = 'none';
+    
+    // Insert the placeholder before the nav element
+    nav.parentNode.insertBefore(navPlaceholder, nav);
+
+    document.addEventListener('scroll', () => {
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition > 80) {
+            nav.classList.add('sticky-header');
+            navPlaceholder.style.display = 'block';
+        } else {
+            nav.classList.remove('sticky-header');
+            navPlaceholder.style.display = 'none';
+        }
+    });
+});
